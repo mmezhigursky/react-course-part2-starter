7e47ecfd87aab25e9fefd34ca5325fcad412f209
max <m.mezhigurskii@gmail.com>
2023-11-29 21:13:49 +0100

    add userpost query

---
+++ src/react-query/hooks/UsePosts.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+  }
+
+const usePosts = (userId:number | undefined) => useQuery<Post[], Error> ({
+    queryKey: userId ? ['users', userId, 'posts'] : ['posts'],
+    queryFn: () => axios
+    .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.data),
+    staleTime: 10 * 10000,
+
+   });
+
+   export default usePosts
